7563ec97cd3b3fcc4bf0dfbf1c7d6285c0ae8719
sathiya12342 <sathiyajayasankar195@gmail.com>
2025-08-25 15:48:57 +0530

    Pet Adoption

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { HashRouter, Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
@@ -31,7 +31,7 @@ function App() {
 
 
   return (
-    <BrowserRouter basename="/PetAdoption-Shop">
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -46,7 +46,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+   </HashRouter>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PetAdoption-Shop/',   // 👈 use your repo name here
+  base: '/PetAdaption-Shop/',   // 👈 use your repo name here
 })
